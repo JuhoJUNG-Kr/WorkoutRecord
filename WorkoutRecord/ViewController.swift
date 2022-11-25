c931a954aa70a85169f589de6263c243e8015e24
Juho Jung <jooho293@gmail.com>
2022-11-25 20:06:42 +0100

    Initial Commit

---
+++ WorkoutRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutRecord
+//
+//  Created by 정주호 on 25/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
